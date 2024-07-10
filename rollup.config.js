1652b4b7dff1906fb3776d14099be2af5ccb16d6
Lionel <fullstackbeaver@gmail.com>
2024-07-10 16:44:39 +0200

    build: update final build

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,19 @@
 // import commonjs   from '@rollup/plugin-commonjs';
 import resolve    from '@rollup/plugin-node-resolve';
-// import { terser } from "rollup-plugin-terser";
+import { terser } from "rollup-plugin-terser";
 import typescript from '@rollup/plugin-typescript';
 
 export default {
   input: 'src/main.ts',
   output: {
-    file: 'bin/ha-config-generator.cjs',
-    format: 'cjs'
+    file: 'ha-config-generator.js',
+    format: 'es'
   },
   plugins: [
     resolve(),
     // commonjs(),
     typescript(),
-    // terser() // Minify the bundle
+    terser() // Minify the bundle
   ],
   external: id => /node_modules/.test(id), // Mark dependencies in node_modules as external
 };
